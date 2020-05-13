926970daeb73c550aab584677e5cbba548a2492c
k.toshiki <ktoshiki0511@yaoo.co.jp>
2020-05-13 01:18:03 +0000

    users_sessions_controller_init

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
+  get '/login', to: "sessions#new"
+  resources :users
   
-  root 'application#hello'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'sessions#new'
 end
