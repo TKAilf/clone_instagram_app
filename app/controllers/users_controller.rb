0cab80619c21747274162380d08544c5cc492223
k.toshiki <ktoshiki0511@yaoo.co.jp>
2020-05-17 01:36:51 +0000

    setup_user_new_form_path

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,15 +3,23 @@ class UsersController < ApplicationController
   end
   
   def create
+    @user = User.new(user_params)
+    if @user.save
+      
+    else
+      render 'new'
+    end
   end
   
   def new
+    @user = User.new
   end
   
   def edit
   end
   
   def show
+    @user = User.find(params[:id])
   end
   
   def update
@@ -20,5 +28,10 @@ class UsersController < ApplicationController
   def destroy
   end
   
+  private
+    
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
   
 end
