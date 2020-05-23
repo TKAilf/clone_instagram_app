bdfe8a27554b31315821b7ff213e7c359ddeed01
k.toshiki <ktoshiki0511@yaoo.co.jp>
2020-05-23 14:08:15 +0000

    finish mailer form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,23 +1,29 @@
 class SessionsController < ApplicationController
-  
+
   def new
   end
-  
+
   def create
     @user = User.find_by(email: params[:session][:email].downcase)
     if @user && @user.authenticate(params[:session][:password])
-      log_in @user
-      params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
-      redirect_back_or(static_pages_home_path)
+      if @user.activated?
+        log_in @user
+        params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
+        redirect_back_or static_pages_home_path
+      else
+        message  = "アカウントが有効化されていません。"
+        message += "有効化リンクをメールで確認してください。"
+        flash[:warning] = message
+        redirect_to root_url
+      end
     else
-      flash.now[:danger] = "メールアドレスかパスワードが違います。"
+      flash.now[:danger] = 'メールアドレスもしくはパスワードが違います。'
       render 'new'
     end
   end
-  
+
   def destroy
     logout if logged_in?
-    redirect_to root_path
+    redirect_to root_url
   end
-  
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,15 +4,15 @@ class UsersController < ApplicationController
   before_action :admin_user,     only: :destroy
   
   def index
-    @users = User.paginate(page: params[:page])
+    @users = User.where(activated: true).paginate(page: params[:page])
   end
   
   def create
     @user = User.new(user_params)
     if @user.save
-      UserMailer.account_activation(@user).deliver_now
+      @user.send_activation_email
       flash[:info] = "アカウント有効化用のメールを送信しました。確認してください。"
-      redirect_to static_pages_home_path
+      redirect_to root_url
     else
       render 'new'
     end
@@ -28,6 +28,7 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
+    redirect_to static_pages_home_path and return unless @user.activated?
   end
   
   def update
