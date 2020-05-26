ea68850b27504a193a5337748efa817e73999354
k.toshiki <ktoshiki0511@yaoo.co.jp>
2020-05-26 00:39:26 +0000

    finish twitter clone model instagram app

--- config/application.rb
+++ config/application.rb
@@ -5,6 +5,7 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+require File.expand_path('../boot', __FILE__)
 
 module CloneInstagramApp
   class Application < Rails::Application
@@ -14,5 +15,7 @@ module CloneInstagramApp
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    # 認証トークンをremoteフォームに埋め込む
+    config.action_view.embed_authenticity_token_in_remote_forms = true
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,15 @@ Rails.application.routes.draw do
   delete '/logout', to: "sessions#destroy"
   get '/signup',  to: 'users#new'
   post '/signup', to: "users#create"
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resources :account_activations, only: [:edit]
   resources :password_resets, only:[:new,:create,:update,:edit]
   resources :microposts,          only: [:create, :destroy]
+  resources :relationships,       only: [:create, :destroy]
   
   root 'sessions#new'
 end
